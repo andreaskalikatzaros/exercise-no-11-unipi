dd2218fc0b868b8a701c678e79559171e9fcd093
andreaskalikatzaros <98468010+andreaskalikatzaros@users.noreply.github.com>
2022-02-17 15:44:27 +0200

    Update exercise 11.py

--- exercise 11.py
+++ exercise 11.py
@@ -1,16 +1,21 @@
 import math
+import numpy as np
 from urllib import request
 from urllib.request import Request, urlopen
 import json
 d=[]
-req = Request('https://drand.cloudflare.com/public/latest', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
-data = urlopen(req).read()
-data=data.decode()
-res = json.loads(data)
-y=len(res['randomness'])-20
-for i in range (y,len(res['randomness'])):
-    d.append(res['randomness'][i])
-k=''.join(d)
+for i in range (2):
+  req = Request('https://drand.cloudflare.com/public/latest', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
+  data = urlopen(req).read()
+  data=data.decode()
+  res = json.loads(data)
+  y=len(res['randomness'])
+  for i in range (0,len(res['randomness'])):
+     d.append(res['randomness'][i])
+     print(res['randomness'][i])
+     
+
+k ="".join(d)
 k = int(k, 16)
 n=16
 p=k/n
